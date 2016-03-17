c96d6b5854df3ea4228932188d2f99d4fc6fad8a
kevinanschau <kevinan@student.ethz.ch>
2016-03-17 13:17:07 +0100

    definition of different tasks to achieve camera pose estimate with 3D-2D point correspondences

---
+++ matlab/simulation.m
@@ -0,0 +1,11 @@
+% generate 3D points
+
+% add noise to true 3d points
+
+% initialiaze camera
+
+% project 3d to 2d points
+
+% add pixel noise to 2d projection
+
+% estimate camera pose with PnP algorithm
