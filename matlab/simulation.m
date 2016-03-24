2a4c19d763431301f0a563897c40592d84dd1e5b
kevinanschau <kevinan@student.ethz.ch>
2016-03-24 12:06:40 +0100

    camera.m class

---
+++ matlab/simulation.m
@@ -0,0 +1,43 @@
+%% 3D Vision Group 1: Using Lines for Pose Estimation
+clear all, close all, clc
+
+%% Load Parameters
+parameterFile = 'parameters.mat';
+load(parameterFile);
+
+%% generate 3D points
+
+% Initialize random number generator
+% rng(0,'twister')
+
+% Generate 3D Pointcloud
+pointcloud3D = Pointcloud3D(numberOfPoints,shape,scale,...
+    anisotropicGaussianMean,anisotropicGaussianVariance);
+
+% Plot the point cloud of the true points
+pointcloud3D.plotTruePointcloud()
+axis vis3d
+
+<<<<<<< HEAD
+% initialize camera
+Cam = Camera(cameraRadius, polarAngleMax);
+
+% plot the camera position and orientation
+visualizeCamera(Cam);
+=======
+% Initialize camera
+T_WC = [[1 0 0; 0 sqrt(3)/2 1/2; 0 -1/2 sqrt(3)/2] [0,1,1]'; zeros(1,3) 1];
+>>>>>>> 687d3f19cd0b8dbe8b730257fb024c374b19218c
+
+% add noise to 3d points
+pointcloud3D.addNoiseToAllPoints(T_WC)
+
+hold on
+
+pointcloud3D.plotNoisyPointcloud()
+
+% project 3d to 2d points
+
+% add pixel noise to 2d projection
+
+% estimate camera pose with PnP algorithm
