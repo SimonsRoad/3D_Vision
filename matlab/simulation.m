2f9f08a8bd0ed51e909d3df30b1294d31defd43c
kevinanschau <kevinan@student.ethz.ch>
2016-04-14 20:55:45 +0200

    Chronological plots in simulation.m

--- matlab/simulation.m
+++ matlab/simulation.m
@@ -29,7 +29,6 @@ camera.pointCloud3D.addNoiseToAllPoints(trueP);
 
 camera.pointCloud3D.computeCameraFrameCoordinates(trueP);
 
-
 %% Project true 3D points to 2D points
 % Project 3d to 2d points
 camera.projectFrom3DTo2D();
@@ -49,7 +48,6 @@ camera.addPixelNoise('gaussian', pixelNoiseMean, pixelNoiseVariance);
 % back transformation from pixel to image coordinates
 camera.transformFromPixelToImage();
 
-
 %% Pose Estimation
 % estimate camera pose with PnP algorithm
 camera.setPnPAlgorithm(pnpAlgorithm);
@@ -62,46 +60,89 @@ disp(['Orientation Error: ' num2str(errorInOrientation) '   [degrees]'])
 
 %% Plots
 % Plot the point cloud of the true points
-fig1 = figure(1)
+fig1 = figure(1);
 camera.pointCloud3D.plotTruePointcloud();
-axis vis3d
+
+% Plot camera true pose with true point cloud
+fig2 = figure(2);
+camera.pointCloud3D.plotTruePointcloud();
+axis equal
 hold on
-
-% Plot the point cloud of the noisy points
-camera.pointCloud3D.plotNoisyPointcloud('false');
-camera.plotConfidenceIntervals();
-
-% Plot camera true pose and estimated pose of the camera
-camera.visualizeCamera(1)
+camera.visualizeTrueCamera(2)
 hold off
 
-% Plot image plane
-fig2 = figure(2)
+% Plot camera true pose, true point cloud and point cloud of the noisy points
+fig3 = figure(3);
+camera.pointCloud3D.plotTruePointcloud();
+axis equal
+hold on
+camera.visualizeTrueCamera(3)
+camera.pointCloud3D.plotNoisyPointcloud('false');
+camera.plotConfidenceIntervals();
+hold off
+
+% Plot true 3D points projected to image plane
+fig4 = figure(4);
 camera.pointCloud2D.plotImagePoints();
 title('Image Plane')
 xlabel('')
 ylabel('')
-axis equal
-hold on
+axis([-.5*xResolution/kx .5*xResolution/kx -.5*yResolution/ky .5*yResolution/ky])
 legend('true 3D projection')
-% pause
-camera.pointCloud2D.plotDistortedImagePoints();
-camera.pointCloud2D.plotBackProjectedImagePoints();
-legend('true 3D projection','true 3D distorted projection','back projection from pixel')
-% pause
-hold off
 
-% Plot pixel plane
-fig3 = figure(3)
-camera.pointCloud2D.plotDistortedPixelPoints();
-title('Pixel space')
+% Plot projected points and distorted points on image plane
+fig5 = figure(5);
+camera.pointCloud2D.plotImagePoints();
+hold on
+camera.pointCloud2D.plotDistortedImagePoints();
+title('Image Plane')
 xlabel('')
 ylabel('')
+axis([-.5*xResolution/kx .5*xResolution/kx -.5*yResolution/ky .5*yResolution/ky])
+legend('true 3D projection','true 3D distorted projection')
+hold off
+
+% Plot projected, distorted and backprojected points on image plane
+fig6 = figure(6);
+camera.pointCloud2D.plotImagePoints();
+hold on
+camera.pointCloud2D.plotDistortedImagePoints();
+camera.pointCloud2D.plotBackProjectedImagePoints();
+title('Image Plane')
+xlabel('')
+ylabel('')
+axis([-.5*xResolution/kx .5*xResolution/kx -.5*yResolution/ky .5*yResolution/ky])
+legend('true 3D projection','true 3D distorted projection','back projection from pixel')
+
+% Plot pixel representation of distorted image plane points
+fig7 = figure(7);
+camera.pointCloud2D.plotDistortedPixelPoints();
+title('Pixel Space')
+xlabel('')
+ylabel('')
+axis([0 xResolution 0 yResolution])
+legend('distorted image points to pixel')
+
+% Plot pixel and noisy pixel points
+fig8 = figure(8);
+camera.pointCloud2D.plotDistortedPixelPoints();
+hold on
+camera.pointCloud2D.plotNoisyPixelPoints();
+title('Pixel Space')
+xlabel('')
+ylabel('')
+axis([0 xResolution 0 yResolution])
+legend('distorted image points to pixel','added pixel noise')
+hold off
+
+% Plot true point cloud, noisy point cloud, true camera pose and estimated
+% camera pose
+fig9 = figure(9);
+camera.pointCloud3D.plotTruePointcloud();
 axis equal
 hold on
-legend('distorted image points to pixel')
-% pause
-camera.pointCloud2D.plotNoisyPixelPoints();
-legend('distorted image points to pixel','added pixel noise')
-% pause
+camera.visualizeTrueCamera(9)
+camera.pointCloud3D.plotNoisyPointcloud('false');
+camera.plotConfidenceIntervals();
+camera.visualizeEstimatedCamera(9)
 hold off
